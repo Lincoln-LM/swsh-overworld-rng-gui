979cc5d53cf49b2e599220cad24a79f3864ed99c
Lincoln-LM <73306575+Lincoln-LM@users.noreply.github.com>
2024-08-07 18:14:48 -0600

    remove non-held advances

--- SWSH_OWRNG_Generator.Core/MenuClose/Generator.cs
+++ SWSH_OWRNG_Generator.Core/MenuClose/Generator.cs
@@ -10,11 +10,8 @@ namespace SWSH_OWRNG_Generator.Core.MenuClose
             {
                 rng.NextInt(91);
             }
-            rng.Next();
-            rng.NextInt(60);
             if (Mode == (byte)MenuCloseType.HoldingDirection)
             {
-                rng.NextInt(360);
             }
             else if (Mode == (byte)MenuCloseType.CaveRegular)
             {
